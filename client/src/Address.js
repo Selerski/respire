ff13938736d8eb514e8fa57c766a26d2e7c4664d
pixelherd <6398412+pixelherd@users.noreply.github.com>
2020-03-07 18:39:08 +0000

    feat: implement address item (eventless)

--- client/src/Address.js
+++ client/src/Address.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Timer from './Timer';
+import TimerBlockButton from './TimerBlockButton'
 import './Address.css';
 
 
 const Address = ({blockedStatus, children, onClick}) => {
   
   const buttonPanel = (blockedStatus === 'notBlocked') 
-    ? <span> <button>block me</button> <Timer /> </span>
-    : <button>unblock me</button>
+    ? <> <button>block</button><Timer /> </>
+    : <TimerBlockButton time={120}/> 
 
   return (
       <div className='address'>
