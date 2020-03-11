b24c6c04b9afe2ad3a3f9148c0f0e92a4a6de4df
Pawel <pawel.pietruszka99@gmail.com>
2020-03-11 11:39:12 +0000

    add: social bar feature

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,30 +1,30 @@
 const Address = require('../models/address');
 
-exports.getAddresses = async (ctx) => {
+exports.getAddresses = async ctx => {
   const addresses = await Address.find();
   ctx.body = addresses;
 };
 
-exports.getAddress = async (ctx) => {
+exports.getAddress = async ctx => {
   const address = await Address.findById(ctx.params.id);
   ctx.body = address;
 };
 
-exports.blockAddress = async (ctx) => {
+exports.blockAddress = async ctx => {
   const address = await Address.findById(ctx.params.id);
   address.blockedStatus = 'Blocked';
   await address.save();
   ctx.body = address;
 };
 
-exports.unblockAddress = async (ctx) => {
+exports.unblockAddress = async ctx => {
   const address = await Address.findById(ctx.params.id);
   address.blockedStatus = 'notBlocked';
   await address.save();
   ctx.body = address;
 };
 
-exports.timeBlockAddress = async (ctx) => {
+exports.timeBlockAddress = async ctx => {
   const address = await Address.findById(ctx.params.id);
   address.blockedStatus = 'timeBlocked';
   address.blockedDate = date;
@@ -33,37 +33,58 @@ exports.timeBlockAddress = async (ctx) => {
   ctx.body = address;
 };
 
+exports.toggleWidget = async ctx => {
+  let input = ctx.params.params.split('&').map((item, index) => {
+    return item.split('=')[1];
+  });
+
+  const [domain, https, port, blockedStatus] = input;
+  const output = { domain, https, port };
+  const address = await this.getOrRegister(output);
+
+  ctx.body = address;
+};
+
 exports.getOrRegister = async ({ https, port, domain, subdomain, ip }) => {
   let mongoAddress;
-  const dbAddress = await Address.findOne({ https, port, domain }, async (err, addr) => {
-    if (!addr) {
-      mongoAddress = new Address({ 
-        https,
-        port,
-        domain,
-        subdomains: [{ name: subdomain, ips: [ip] }],
-      });
-      await mongoAddress.save();
-      // console.log('New address saved!: ', mongoAddress.domain);
+  const dbAddress = await Address.findOne(
+    { https, port, domain },
+    async (err, addr) => {
+      if (!addr) {
+        mongoAddress = new Address({
+          https,
+          port,
+          domain,
+          subdomains: [{ name: subdomain, ips: [ip] }]
+        });
+        await mongoAddress.save();
+      }
     }
-  });
+  );
   if (mongoAddress) return mongoAddress;
-  
-  if (dbAddress.subdomains.filter((subd) => subd.name === subdomain).length === 0) { // make this a "find"
-    dbAddress.subdomains = [...dbAddress.subdomains, { name: subdomain, ips: [ip] }];
+
+  if (
+    dbAddress.subdomains.filter(subd => subd.name === subdomain).length === 0
+  ) {
+    dbAddress.subdomains = [
+      ...dbAddress.subdomains,
+      { name: subdomain, ips: [ip] }
+    ];
     dbAddress.save();
-  } 
-  // else {
-  //   console.log('Already saved', dbAddress.domain);
-  // }
+  }
+
   return dbAddress;
 };
 
 exports.unBlockExpiredAddress = async () => {
-  const addresses = await Address.find({ blockedStatus: 'timeBlocked'});
-  addresses.forEach((dbAddress) => {
-    if (Date.parse(dbAddress.blockedDate) - Date.now() + dbAddress.blockedTimePeriod < 0) {
-      // console.log(dbAddress);
+  const addresses = await Address.find({ blockedStatus: 'timeBlocked' });
+  addresses.forEach(dbAddress => {
+    if (
+      Date.parse(dbAddress.blockedDate) -
+        Date.now() +
+        dbAddress.blockedTimePeriod <
+      0
+    ) {
       exports.unblockAddress({ params: { id: dbAddress._id } });
     }
   });
--- routers.js
+++ routers.js
@@ -1,5 +1,5 @@
 const Router = require('koa-router');
-const { getAddresses, blockAddress, timeBlockAddress, getAddress, unblockAddress } = require('./controllers/addressController');
+const { getAddresses, blockAddress, timeBlockAddress, getAddress, unblockAddress, toggleWidget, toggleAllWidgets } = require('./controllers/addressController');
 
 const router = new Router();
 router.get('/addresses', getAddresses);
@@ -7,5 +7,6 @@ router.get('/addresses/:id', getAddress);
 router.put('/addresses/:id/block', blockAddress);
 router.put('/addresses/:id/timeblock', timeBlockAddress);
 router.put('/addresses/:id/unblock', unblockAddress);
+router.put('/addresses/widget/:params', toggleWidget);
 
 module.exports = router;
